c0a432acb44d6c831f6125934ab97e9fd8b40f56
Hasan-aa03 <HasanBMRM@Gmail.com>
2024-10-09 23:27:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        function toProdukt(nummer){
+            window.location = `Produktside${nummer}.html`;
+        };
+        function gohome(){
+            window.location="Hovedside.html"
+        }
+ 
